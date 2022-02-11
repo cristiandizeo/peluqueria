8421a61d748617171fcbca8f18be412a618a2368
Cristian <dizeoc@gmail.com>
2022-02-11 11:26:38 -0300

    Seccion 42

---
+++ views/auth/olvide-password.php
@@ -0,0 +1,20 @@
+<h1 class="nombre-pagina">Olvide mi contraseña</h1>
+<p class="descripcion-pagina">Restablece tu contraseña escribiendo tu email a continuación</p>
+
+<form action="/olvide" class="formulario" method="POST">
+    <div class="campo">
+        <label for="email">Email</label>
+        <input 
+        type="email"
+        id="email"
+        name="email"
+        placeholder="Tu email"/>
+    </div>
+
+    <input type="submit" class="boton" value="Enviar">
+</form>
+
+<div class="acciones">
+    <a href="/">¿Ya tienes una cuenta? Iniciá sesión</a>
+    <a href="/crear-cuenta">Registrarse</a>
+</div>
