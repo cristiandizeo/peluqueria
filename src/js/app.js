102ae2e197f81edae76b6bebcd889150bb58f396
Cristian <dizeoc@gmail.com>
2022-03-08 13:16:50 -0300

    Sec 49 ok

--- src/js/app.js
+++ src/js/app.js
@@ -1,46 +1,88 @@
 let paso = 1;
+const pasoInicial = 1;
+const pasoFinal = 3;
 
-document.addEventListener('DOMContentLoaded', function(){
-    iniciarApp();
+document.addEventListener("DOMContentLoaded", function () {
+  iniciarApp();
 });
 
-
-function iniciarApp(){
-    mostrarSeccion();
-    tabs(); //Cambia las secciones al presionar tabs)
+function iniciarApp() {
+  mostrarSeccion(); // Mostrar/ocultar secciones
+  tabs(); // Cambia las secciones al presionar tabs
+  botonesPaginador(); // Agrega o quita botones paginador
+  paginaSiguiente();
+  paginaAnterior();
 }
 
-function mostrarSeccion(){
-    //Ocultar la seccion mostrar
-    const seccionAnterior = document.querySelector('.mostrar')
-    if(seccionAnterior){
-    seccionAnterior.classList.remove('mostrar');
+function mostrarSeccion() {
+  //Ocultar la seccion mostrar
+  const seccionAnterior = document.querySelector(".mostrar");
+  if (seccionAnterior) {
+    seccionAnterior.classList.remove("mostrar");
+  }
+  //Seleccionar la seccion/paso
+  const pasoSelector = `#paso-${paso}`;
+  const seccion = document.querySelector(pasoSelector);
+  seccion.classList.add("mostrar");
+
+  //Quitar la clase actual al tab anterior
+  const tabAnterior = document.querySelector(".actual");
+  if (tabAnterior) {
+    tabAnterior.classList.remove("actual");
+  }
+
+  //Color tab seleccionado
+  const tab = document.querySelector(`[data-paso="${paso}"]`);
+  tab.classList.add("actual");
 }
-    //Seleccionar la seccion/paso
-    const pasoSelector = `#paso-${paso}`;
-    const seccion = document.querySelector(pasoSelector);
-    seccion.classList.add('mostrar');
 
-    //Quitar la clase actual al tab anterior
-    const tabAnterior = document.querySelector('.actual');
-    if(tabAnterior){
-        tabAnterior.classList.remove('actual');
-    }
+function tabs() {
+  const botones = document.querySelectorAll(".tabs button");
 
-    //Color tab seleccionado
-    const tab = document.querySelector(`[data-paso="${paso}"]`);
-    tab.classList.add('actual');
+  botones.forEach((boton) => {
+    boton.addEventListener("click", function (e) {
+      paso = parseInt(e.target.dataset.paso);
 
+      mostrarSeccion();
+
+      botonesPaginador();
+    });
+  });
 }
 
-function tabs(){
-    const botones = document.querySelectorAll('.tabs button');
+function botonesPaginador() {
+  const paginaAnterior = document.querySelector("#anterior");
+  const paginaSiguiente = document.querySelector("#siguiente");
 
-    botones.forEach( boton =>{
-        boton.addEventListener('click', function(e){
-            paso = parseInt(e.target.dataset.paso);
+  if (paso === 1) {
+    paginaAnterior.classList.add("ocultar");
+    paginaSiguiente.classList.remove("ocultar");
+  } else if (paso === 3) {
+    paginaAnterior.classList.remove("ocultar");
+    paginaSiguiente.classList.add("ocultar");
+  } else {
+    paginaAnterior.classList.remove("ocultar");
+    paginaSiguiente.classList.remove("ocultar");
+  }
 
-            mostrarSeccion();
-        });
-    })
+  mostrarSeccion();
+}
+
+function paginaAnterior() {
+  const paginaAnterior = document.querySelector("#anterior");
+  paginaAnterior.addEventListener("click", function () {
+    if (paso <= pasoInicial) return;
+    paso--;
+
+    botonesPaginador();
+  });
+}
+function paginaSiguiente() {
+  const paginaSiguiente = document.querySelector("#siguiente");
+  paginaSiguiente.addEventListener("click", function () {
+    if (paso >= pasoFinal) return;
+    paso++;
+
+    botonesPaginador();
+  });
 }
