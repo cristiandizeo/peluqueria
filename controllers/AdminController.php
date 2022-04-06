bf014e610b04803244433d8086cd0743b6cfe0a0
Cristian Dizeo <dizeoc@gmail.com>
2022-04-06 17:39:32 -0300

    fn fiter fechas

--- controllers/AdminController.php
+++ controllers/AdminController.php
@@ -11,6 +11,9 @@ class AdminController
     {
         session_start();
 
+        $fecha = date('Y-m-d');
+        debuguear($fecha);
+
         // Consultar la base de datos
         $consulta = "SELECT citas.id, citas.hora, CONCAT( usuarios.nombre, ' ', usuarios.apellido) as cliente, ";
         $consulta .= " usuarios.email, usuarios.telefono, servicios.nombre as servicio, servicios.precio  ";
@@ -21,15 +24,14 @@ class AdminController
         $consulta .= " ON citasServicios.citaId=citas.id ";
         $consulta .= " LEFT OUTER JOIN servicios ";
         $consulta .= " ON servicios.id=citasServicios.servicioId ";
-        // $consulta .= " WHERE fecha =  '${fecha}' ";
+        $consulta .= " WHERE fecha =  '${fecha}' ";
 
         $citas = AdminCita::SQL($consulta);
 
-        debuguear($citas);
-
         $router->render('admin/index', [
             'nombre' => $_SESSION['nombre'],
-            'citas => $citas'
+            'citas' => $citas,
+            'fecha' => $fecha
         ]);
     }
 }
--- views/admin/index.php
+++ views/admin/index.php
@@ -9,11 +9,43 @@ include_once __DIR__ . '/../templates/barra.php';
         <div class="campo">
             <label for="fecha">Fecha</label>
             <input 
-            type="date"
-            id="fecha"
+            type="date" 
+            id="fecha" 
             name="fecha"
-            />
+            value="<?php echo $fecha;?>"
+             />
         </div>
     </form>
 </div>
-<div class="citas-admin"></div>
+<div class="citas-admin">
+    <ul class="citas">
+        <?php
+        $idCita = 0;
+        foreach ($citas as $key => $cita) {
+            if ($idCita !== $cita->id) {
+                $total = 0;
+
+                $idCita = $cita->id;
+        ?>
+                <li>
+                    <p>ID: <span><?php echo $cita->id; ?></span></p>
+                    <p>Hora: <span><?php echo $cita->hora; ?></span></p>
+                    <p>Cliente: <span><?php echo $cita->cliente; ?></span></p>
+                    <p>Email: <span><?php echo $cita->email; ?></span></p>
+                    <p>Telefono: <span><?php echo $cita->telefono; ?></span></p>
+                    <h3>Servicios</h3>
+                <?php } //Fin de if 
+            $total += $cita->precio;
+                ?>
+                <p class="servicio"><?php echo $cita->servicio . ": $" . $cita->precio; ?></p>
+                <?php
+                $actual = $cita->id;
+                $proximo = $citas[$key + 1]->id ?? 0;
+
+                if (esUltimo($actual, $proximo)) { ?>
+                    <p class="total">Total: <span>$<?php echo $total; ?></span></p>
+            <?php }
+            }
+            ?>
+    </ul>
+</div>
